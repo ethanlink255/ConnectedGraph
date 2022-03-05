66591c1090a7cb8c0114337baeac8cd6986350bd
Ethan Link <ethanlink255@gmail.com>
2022-03-04 19:38:38 -0500

    Finished Source Code

--- ConnectedGraph.cpp
+++ ConnectedGraph.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
+// Element structure that holds each node in the graph
 struct Element{
-    int head;
-    Element* tail;
+    int head; //value
+    Element* tail; //pointer to next element
 
     Element(int h, Element* t){
         head = h;
@@ -16,12 +17,14 @@ struct Element{
     }
 };
 
+// class that holds all elements
 class Graph{
     private:
         Element *HNodes;
 	int els; //root nodes
 
     public:
+        // constructor
         Graph(int n){
             els = n;
             HNodes = (Element*) malloc(sizeof(Element) * (n - 1));
@@ -31,7 +34,10 @@ class Graph{
             }
         }
 
+        // add element to graph
         void add(int n, int v){
+            
+            // if its at the end, just add the element, else search for required node
             if(HNodes[n].tail == nullptr) HNodes[n].tail = new Element(v, nullptr);
             else{
                 Element *node = &HNodes[n];
@@ -41,7 +47,7 @@ class Graph{
 
                 node->tail = new Element(v, nullptr);
             }
-
+            // if its at the end, just add the element, else search for required node
             if(HNodes[v].tail == nullptr) HNodes[v].tail = new Element(n, nullptr);
             else{
                 Element *node = &HNodes[v];
@@ -54,15 +60,21 @@ class Graph{
 
         }
 
+        //removes connections between nodes 
         void remove(int n, int v){
             Element *pnode = &HNodes[n];
             Element *cnode = HNodes[n].tail;
             Element *rnode = &HNodes[v];
             Element *fnode = HNodes[v].tail;
 
+            //checks for connection, removes node from adj list and stiches list back together.
             while (1){
                 if(cnode->head == v){
-                    pnode->tail = nullptr;
+                    if(cnode->tail != nullptr){
+                        pnode->tail = cnode->tail;
+                    }else{
+                        pnode->tail = nullptr;
+                    }
                     delete cnode;
                     break;
                 } 
@@ -71,20 +83,24 @@ class Graph{
                     cnode = pnode->tail;
                 } else break;
             }
-
              while (1){
                 if(fnode->head == v){
-                    rnode->tail = nullptr;
+                    if(fnode->tail != nullptr){
+                        rnode->tail = fnode->tail;
+                    }else{
+                        rnode->tail = nullptr;
+                    }
                     delete fnode;
                     break;
                 } 
                 if(fnode->tail != nullptr){
-                    rnode = cnode;
-                    cnode = rnode->tail;
+                    rnode = fnode;
+                    fnode = rnode->tail;
                 } else break;
             }
         }
 
+        //prints adjacency list view
         void prettyprint(){
             for(int i = 0; i < els; i++){
                 cout << i << ": ";
@@ -99,8 +115,9 @@ class Graph{
             }
         }
 
+        //implementation of recursive dfs algorithm
         void dfs(int v, int* mark, int* visited, int found){
-            mark[v] = 1; //BIG BAD
+            mark[v] = 1;
             Element* cnode = &HNodes[v];
             while (cnode->tail != nullptr){
                 cnode = cnode->tail;
@@ -111,40 +128,46 @@ class Graph{
             }
         }
 
+        //dfs overloader that initializes helper vars. Should be the one that's called.
         int* dfs(int v){ 
             int *mark = (int*) malloc(sizeof(int) * (els));
-            int *visited = (int*) malloc(sizeof(int) * (els));    //this
+            int *visited = (int*) malloc(sizeof(int) * (els)); 
             for(int i = 0; i < els; i++) {
-		mark[i] = 0;
-		visited[i] = -1;
-	    }
-	    visited[0] = v;
+                mark[i] = 0;
+                visited[i] = -1;
+            }
+	        visited[0] = v;
             dfs(v, mark, visited, 1);
             delete mark;
 
             return visited;
         }
 
-/*        void components(){
-		for(int i = 0; i < els; i++){
-		/*	int* res = dfs(i);
-			cout << i << ": ";
-			int r = 0;
-			while (r < els && res[r] != -1){
-				cout << res[r++];
-			}
-			cout << endl;
+        //calculates connected graphs
+        void components(){
+            for(int i = 0; i < els; i++){
+                int* res = dfs(i);
+                cout << i << ": ";
+                int r = 0;
+                cout << "{";
+                //loops through dfs result until -1 terminator is encountered
+                while (r < els && res[r] != -1){
+                    cout << res[r++] << ", ";
+                }
+                cout << "\b\b}" << endl;
 
-		}
-        }*/
+            }
+        }
 
         int size(){
             return els;
         }
 
-
-
         Graph(){ };
+
+        ~Graph(){
+            delete HNodes;
+        }
 };
 
 
@@ -166,15 +189,25 @@ int main(){
         } else break;
     }
 
+    cout << endl << "Direct Connections" << endl;
     g.prettyprint();
 
-    cout << "Result of DFS, with starting vertex of 2" << endl;
+    cout << endl << "Result of DFS, with starting vertex of 2" << endl;
     int *search = g.dfs(2);
     for(int i = 0; i < g.size(); i++ ){
-        cout << search[i] << " ";
+        int res = search[i];
+        if(search[i] != -1){
+            cout << search[i] << " ";
+        }
     }
-
-//    g.components();
+    cout << endl << endl;
+    cout << "Connected Components" << endl;
+    g.components();
 
     cout << endl;
+    g.remove(1,3);
+    g.remove(4,3);
+    cout << "Direct Connections after removal of connection between {1,3} and {4,3}" << endl;
+    g.prettyprint();
+
 }
